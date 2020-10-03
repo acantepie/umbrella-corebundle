1f6713dc0fa4c91271d8eb292f0bef3a200bcf03
Cantepie <adrien.cantepie@6tm.com>
2020-10-03 03:20:33 +0200

    Bundle config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Umbrella\CoreBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -18,44 +19,31 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('umbrella_core');
-        $treeBuilder
-            ->getRootNode()
-            ->append($this->webpackNode())
-            ->append($this->redisNode());
+        $rootNode = $treeBuilder->getRootNode();
+
+        $this->addwebpackSection($rootNode);
+        $this->addRedisSection($rootNode);
+
         return $treeBuilder;
     }
 
-    private function webpackNode()
+    private function addwebpackSection(ArrayNodeDefinition $rootNode)
     {
-        $treeBuilder = new TreeBuilder('webpack');
-        $webpackNode = $treeBuilder->getRootNode()->addDefaultsIfNotSet();
-        $webpackNode->children()
-            ->booleanNode('dev_server_enable')
-                ->defaultFalse()
-                ->end()
-            ->scalarNode('dev_server_host')
-                ->defaultValue('http://127.0.0.1')
-                ->end()
-            ->integerNode('dev_server_port')
-                ->defaultValue(9000)
-                ->end();
-        return $webpackNode;
+        $rootNode->children()
+            ->arrayNode('webpack')->addDefaultsIfNotSet()
+            ->children()
+                ->booleanNode('dev_server_enable')->defaultFalse()->end()
+                ->scalarNode('dev_server_host')->defaultValue('http://127.0.0.1')->end()
+                ->integerNode('dev_server_port')->defaultValue(9000)->end();
     }
 
-    private function redisNode()
+    private function addRedisSection(ArrayNodeDefinition $rootNode)
     {
-        $treeBuilder = new TreeBuilder('redis');
-        $node = $treeBuilder->getRootNode()->addDefaultsIfNotSet();
-        $node->children()
-            ->scalarNode('host')
-                ->defaultValue('127.0.0.1')
-                ->end()
-            ->scalarNode('port')
-                ->defaultValue('6379')
-                ->end()
-            ->scalarNode('db')
-                ->defaultValue('0')
-                ->end();
-        return $node;
+        $rootNode->children()
+            ->arrayNode('redis')->addDefaultsIfNotSet()
+            ->children()
+                ->scalarNode('host')->defaultValue('127.0.0.1')->end()
+                ->scalarNode('port')->defaultValue('6379')->end()
+                ->scalarNode('db')->defaultValue('0')->end();
     }
 }
